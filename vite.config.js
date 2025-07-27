6f169931af00e80a71f4387bada8b29f2352c29f
Galeria-Secreta <galeriasecretamocambique@gmail.com>
2025-07-27 08:23:47 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: 5173,
-    host: true
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true
-  },
-  define: {
-    global: 'globalThis'
-  }
+  plugins: [react()],
 })
